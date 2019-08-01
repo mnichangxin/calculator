d4e63d110ec123df65c85c56a3f12a1b443957ca
mnichangxin <mnichangxin@163.com>
2019-08-02 01:30:31 +0800

    :sparkles: Add feature

--- calculator/ViewController.h
+++ calculator/ViewController.h
@@ -1,12 +1,17 @@
 #import <UIKit/UIKit.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
 @interface ViewController : UIViewController
 
+@property (nonatomic) int currentQuestionIndex;
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
 @property (nonatomic, weak) IBOutlet UILabel *questionLabel;
-@property (nonatomic, weak) IBOutlet UILabel *AnswerLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+@property (nonatomic, weak) IBOutlet UIButton *showQuestionButton;
+@property (nonatomic, weak) IBOutlet UIButton *showAnswerButton;
+
+- (IBAction)showQuestion:(id)sender;
+- (IBAction)showAnswer:(id)sender;
 
 @end
-
-NS_ASSUME_NONNULL_END
--- calculator/ViewController.m
+++ calculator/ViewController.m
@@ -1,13 +1,27 @@
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
-
 @implementation ViewController
 
-- (void) viewDidLoad {
-    [super viewDidLoad];
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.questions = @[@"1 + 1 = ?", @"Where's the capital of China?", @"From what's cognac made?"];
+        self.answers = @[@"2", @"Beijing", @"Grapes"];
+    }
+    return self;
+}
+- (IBAction)showQuestion:(id)sender {
+    self.currentQuestionIndex++;
+    if (_currentQuestionIndex == [self.questions count]) {
+        self.currentQuestionIndex = 0;
+    }
+    NSString *question = [self.questions objectAtIndex:self.currentQuestionIndex];
+    self.questionLabel.text = question;
+    self.answerLabel.text = @"???";
+}
+- (IBAction)showAnswer:(id)sender {
+    NSString *answer = [self.answers objectAtIndex:self.currentQuestionIndex];
+    self.answerLabel.text = answer;
 }
 
 @end
