9d3a4dfc50cf58c227ab3edb6fcfbbd6502a3343
mnichangxin <mnichangxin@163.com>
2019-11-04 23:50:41 +0800

    :art: Rebuild code

--- calculator/Controllers/MainViewController.h
+++ calculator/Controllers/MainViewController.h
@@ -1,27 +1,5 @@
 #import <UIKit/UIKit.h>
 
-//typedef enum {
-//    OperateTypeNone = 0,
-//    OperateTypeAddition = 1,
-//    OperateTypeSubtraction = 2,
-//    OperateTypeMultiplication = 3,
-//    OperateTypeDivision = 4
-//} OperateType;
-//
-//@interface MainViewController : UIViewController
-//
-//@property (nonatomic, copy) NSString *prevText;
-//@property (nonatomic, copy) NSString *text;
-//@property (nonatomic, copy) NSString *displayText;
-//@property (nonatomic, assign) OperateType operateType;
-//@property (nonatomic, assign) BOOL isEqual;
-//@property (nonatomic, weak) IBOutlet UILabel *display;
-//@property (nonatomic, strong) IBOutletCollection(UIButton) NSArray *buttons;
-//
-//- (IBAction)buttonClick:(id)sender;
-//
-//@end
-
 @interface MainViewController : UIViewController
 
 @end
