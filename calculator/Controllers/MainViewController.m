77aa040c90e033007c48f21a4ee3511a286e613c
mnichangxin <mnichangxin@163.com>
2019-11-05 23:37:02 +0800

    :art: Delete invalid code

--- calculator/Controllers/MainViewController.m
+++ calculator/Controllers/MainViewController.m
@@ -14,21 +14,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [[self view] setBackgroundColor:UIColor.blackColor];
-//    [[self view] addSubview:[[[ScreenViewController alloc] init] view]];
-//    [self setView:[[ScreenViewController alloc] init]];
-    
-//    UICollectionViewFlowLayout l;;;;;;;;;ll;l*layout = [[UICollectionViewFlowLayout alloc] init];
-//    [layout setScrollDirection:UICollectionViewScrollDirectionHorizontal];
-//    [layout setItemSize:CGSizeMake(100, 100)];
-//    [layout setMinimumInteritemSpacing:10.0];
-//
-//    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:[[self view] frame] collectionViewLayout:layout];
-//    [collectionView setDelegate:self];
-//    [collectionView setDataSource:self];
-//    [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cellid"];
-    
-//    [[self view] addSubview:collectionView];
-//    [[self view] addSubview:[[[ScreenViewController alloc] init] view]];
 }
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
     return 2;
