228f7e86d9e5af0dac5e1e4a72630e5408da0318
mnichangxin <mnichangxin@163.com>
2019-11-10 23:49:29 +0800

    :lipstick: Update layout style

--- calculator/Controllers/ButtonCollectionView.m
+++ calculator/Controllers/ButtonCollectionView.m
@@ -21,12 +21,18 @@
 }
 
 - (void)layoutSubviews {
-    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width / 4, self.frame.size.height / 5)];
-    [button setTitle:@"1" forState:UIControlStateNormal];
-    [button.layer setBorderColor:UIColor.blackColor.CGColor];
-    [button.layer setBorderWidth:1];
-    [self addSubview:button];
-    
+    CGFloat margin = 10.f;
+    CGFloat width = (self.frame.size.width - margin * 5) / 4;
+    for (int i = 1; i <= 20; i++) {
+        CGFloat x = i % 4 > 0 ? width * (i % 4 - 1) + margin * (i % 4) : width * 3 + margin * 4;
+        CGFloat y = i % 4 > 0 ? width * (i / 4) + margin * (i / 4 + 1) : width * (i / 4 - 1) + margin * (i / 4);
+        UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(x, y, width, width)];
+        [button setTitle:[NSString stringWithFormat:@"%d", i] forState:UIControlStateNormal];
+        [button.layer setBorderColor:UIColor.blackColor.CGColor];
+        [button.layer setBorderWidth:1];
+        [button.layer setCornerRadius:width / 2];
+        [self addSubview:button];
+    }
 }
 
 @end
