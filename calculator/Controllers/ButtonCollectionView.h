61ed6af4bce8f2598d24382abce33d589c8619c1
mnichangxin <mnichangxin@163.com>
2019-11-24 23:56:12 +0800

    Add models

--- calculator/Controllers/ButtonCollectionView.h
+++ calculator/Controllers/ButtonCollectionView.h
@@ -1,5 +1,8 @@
 #import <UIKit/UIKit.h>
 
+
 @interface ButtonCollectionView : UIView
+
+//@property (nonatomic, strong) NSArray *buttons = @[@{@"tag": @118, @"title": @"AC", @"color": @""}, nil];
 
 @end
--- calculator/Models/Button.m
+++ calculator/Models/Button.m
@@ -2,5 +2,11 @@
 
 @implementation Button
 
+- (void)initWithTag:(NSNumber *) tag andTitle:(NSString *)title andColor:(UIColor *)color {
+    [self setTag:tag];
+    [self setTitle:title];
+    [self setColor:color];
+}
+
 @end
 
